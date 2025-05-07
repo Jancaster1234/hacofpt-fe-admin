65620238005561380d3a6bc4bcfcff84bfb39d35
Jancaster1234 <122957619+Jancaster1234@users.noreply.github.com>
2025-05-07 16:53:23 +0700

    fix: update modal background opacity and transition properties for consistency

--- src/components/common/ApiResponseModal.tsx
+++ src/components/common/ApiResponseModal.tsx
@@ -93,7 +93,7 @@ export default function ApiResponseModal({
             // leaveFrom="opacity-100"
             // leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-black/25 dark:bg-black/40 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           {/* This element is to trick the browser into centering the modal contents. */}
@@ -106,11 +106,11 @@ export default function ApiResponseModal({
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            // enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            // enterTo="opacity-100 translate-y-0 sm:scale-100"
             leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            // leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            // leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div
               className={`inline-block transform overflow-hidden rounded-lg text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:align-middle ${getBgColor()}`}
--- src/components/header/NotificationDetailModal.tsx
+++ src/components/header/NotificationDetailModal.tsx
@@ -34,11 +34,11 @@ export default function NotificationDetailModal({
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
+          //   enterFrom="opacity-0"
+          //   enterTo="opacity-100"
           leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          //   leaveFrom="opacity-100"
+          //   leaveTo="opacity-0"
         >
           <div className="fixed inset-0 bg-black/30" />
         </Transition.Child>
